f1701fc632a046c4ec9afc7a8e27e725818a1007
Ana <analujauregui@gmail.com>
2017-11-02 21:07:24 -0600

    append inventory items to page

--- public/scripts.js
+++ public/scripts.js
@@ -7,6 +7,24 @@ $(document).ready(() => {
 const fetchInventory = () => {
   fetch('/api/v1/inventory')
     .then(response => response.json())
-    .then(response => console.log(response))
+    .then(response => displayInventory(response))
     .catch(error => console.log(error))
+}
+
+const displayInventory = (items) => {
+  items.map(item => {
+    console.log(item);
+    let name = item.title;
+    let description = item.description;
+    let img = item.picture_url;
+    let price = JSON.parse(item.price).toFixed(2);
+
+    $('.item-list').append(`<div>
+      <h3>${name}</h3>
+      <img src=${img} />
+      <p>${description}</p>
+      <p>price: $${price}</p>
+    </div>`)
+  })
+
 }
